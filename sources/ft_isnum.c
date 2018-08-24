bcd8ef9a879c6da43e1878f22296c8a6fac30db2
VBrazhnik <vbrazhnikdev@gmail.com>
2018-08-24 18:33:51 +0300

    ft_isnum() was updated

--- sources/ft_isnum.c
+++ sources/ft_isnum.c
@@ -15,8 +15,10 @@
 t_bool	ft_isnum(char *str, int base)
 {
 	size_t	i;
+	size_t	digits;
 
 	i = 0;
+	digits = 0;
 	while (ft_isspace(str[i]))
 		i++;
 	if (base != 10 && !ft_isprefix(&str[i], base))
@@ -28,6 +30,9 @@ t_bool	ft_isnum(char *str, int base)
 	else if (base == 10 && (str[i] == '-' || str[i] == '+'))
 		i++;
 	while (ft_isdigit_base(str[i], base) >= 0)
+	{
 		i++;
-	return ((!str[i]) ? true : false);
+		digits++;
+	}
+	return ((!str[i] && digits) ? true : false);
 }
