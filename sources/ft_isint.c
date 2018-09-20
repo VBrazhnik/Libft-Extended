f44fbd4b1a3ecf665810e33488a02f2caf3b745a
VBrazhnik <vbrazhnikdev@gmail.com>
2018-09-20 03:05:40 +0300

    ft_isint() was updated

--- sources/ft_isint.c
+++ sources/ft_isint.c
@@ -16,10 +16,12 @@ t_bool	ft_isint(const char *str)
 {
 	unsigned int	result;
 	unsigned int	border;
-	size_t			i;
+	int				i;
 	int				sign;
+	int				digits;
 
 	result = 0;
+	digits = 0;
 	border = (unsigned int)(FT_INT_MAX / 10);
 	i = 0;
 	while (ft_isspace(str[i]))
@@ -27,15 +29,14 @@ t_bool	ft_isint(const char *str)
 	sign = (str[i] == '-') ? -1 : 1;
 	if (str[i] == '-' || str[i] == '+')
 		i++;
-	while (ft_isdigit(str[i]))
+	while (ft_isdigit(str[i]) && ++digits)
 	{
-		if ((result > border || (result == border && (str[i] - '0') > 7))
-															&& sign == 1)
-			return (false);
-		else if ((result > border || (result == border && (str[i] - '0') > 8))
-																&& sign == -1)
+		if (((result > border || (result == border && (str[i] - '0') > 7))
+				&& sign == 1)
+			|| ((result > border || (result == border && (str[i] - '0') > 8))
+				&& sign == -1))
 			return (false);
 		result = result * 10 + (str[i++] - '0');
 	}
-	return (!str[i]);
+	return (!str[i] && digits);
 }
