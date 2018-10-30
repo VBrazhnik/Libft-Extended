27727964cfb1bf8e316f167eaf8717374bd29ae8
VBrazhnik <vbrazhnikdev@gmail.com>
2018-10-30 20:59:39 +0200

    ft_signedlen.c and ft_unsignedlen.c were updated

--- sources/ft_signedlen.c
+++ sources/ft_signedlen.c
@@ -17,5 +17,5 @@ size_t	ft_signedlen(intmax_t number, unsigned int base)
 	size_t	size;
 
 	size = ft_unsignedlen((uintmax_t)FT_ABS(number), base);
-	return (size + ((number < 0) ? 1 : 0));
+	return (size + ((number < 0 && size) ? 1 : 0));
 }
--- sources/ft_unsignedlen.c
+++ sources/ft_unsignedlen.c
@@ -16,8 +16,13 @@ size_t	ft_unsignedlen(uintmax_t number, unsigned int base)
 {
 	size_t	size;
 
-	size = 1;
-	while ((number /= base))
-		size++;
+	if (base >= 2)
+	{
+		size = 1;
+		while ((number /= base))
+			size++;
+	}
+	else
+		size = 0;
 	return (size);
 }
